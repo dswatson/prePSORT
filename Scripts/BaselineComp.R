8104ba4be2651d28d7c74245703a326eca3e449e
dswatson <david.s.watson11@gmail.com>
2016-08-18 15:51:39 +0100

    maxit update

--- Scripts/BaselineComp.R
+++ Scripts/BaselineComp.R
@@ -8,28 +8,36 @@ library(dplyr)
 # Phenotypic data
 pheno <- read_csv('Clinical_factors_Pre_PSORT_Amy_v7_15_8_16.csv')
 pheno <- subset(pheno, time != 'wk12')
+pheno <- pheno %>%
+  mutate(wk0.Delta_PASI = ifelse(time == 'wk0', Delta_PASI, 0),
+         wk1.Delta_PASI = ifelse(time == 'wk1', Delta_PASI, 0))
 
 # Genotypic data
 t2g <- read_tsv('Transcripts.tsv')
-dir <- paste(getwd(), 'LesionalSkin', sep='/')  # Or whatever
+dir <- paste(getwd(), 'LesionalSkin', sep='/')
 files <- file.path(dir, pheno$sample, 'abundance.tsv')
 txi <- tximport(files, type='kallisto', tx2gene=t2g, reader=read_csv)
 
 # Normalise and filter data before running SVA
-dds <- DESeqDataSetFromTximport(txi, colData=pheno, design= ~ 1)
-dds <- estimateSizeFactors(dds)
-counts <- counts(dds, normalized=TRUE)
-counts <- counts[rowVars(counts) > 1, ]
+dat <- DESeqDataSetFromTximport(txi, colData=pheno, design= 
+                                ~ 0 + subject + time + wk1.Delta_PASI)
+dat <- estimateSizeFactors(dat)
+dat <- counts(dat, normalized=TRUE)
+dat <- dat[rowVars(dat) > 1, ]
 
 # Run SVA
-mod <- model.matrix(~ 0 + subject + time + time:Delta_PASI, data=pheno)
-mod <- mod[, colnames(mod) != 'timewk0:Delta_PASI']
-mod0 <- mod[, colnames(mod) != 'timewk1:Delta_PASI']
-svobj <- svaseq(counts, mod, mod0)
-mod <- cbind(mod, svobj$sv)
+mod <- model.matrix(~ 0 + subject + time + wk1.Delta_PASI, data=pheno)
+mod0 <- model.matrix(~ 0 + subject + time, data=pheno)
+svobj <- svaseq(dat, mod, mod0)
+pheno <- cbind(pheno, svobj$sv)
+colnames(pheno)[51:53] <- paste0('SV', 1:3)
 
 # Differential expression
+dds <- DESeqDataSetFromTximport(txi, colData=pheno, design= ~ 0 + subject + time + 
+                                SV1 + SV2 + SV3 + wk1.Delta_PASI)
+dds <- estimateSizeFactors(dds)
 dds <- estimateDispersions(dds)
-dds <- nbinomWaldTest(dds, modelMatrix=mod, betaPrior=FALSE)
-res <- na.omit(data.frame(results(dds, filterfun=ihw, name='timewk1.Delta_PASI')))
-sum(res$padj < 0.05)
+dds <- nbinomWaldTest(dds, betaPrior=FALSE, maxit=150)
+res <- na.omit(data.frame(results(dds, filterfun=ihw)))
+
+
