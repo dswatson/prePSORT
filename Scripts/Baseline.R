64adc0cd6adc13ef33cb5c1e49a169de3dffc694
dswatson <david.s.watson11@gmail.com>
2016-09-13 10:01:05 +0100

    full baseline

---
+++ Scripts/Baseline.R
@@ -0,0 +1,51 @@
+# Load libraries
+library(data.table)
+library(tximport)
+library(sva)
+library(DESeq2)
+library(dplyr)
+
+# Prep data
+pheno <- read.csv('Clinical_factors_Pre_PSORT_Amy_v7_15_8_16.csv', stringsAsFactors=FALSE)
+pheno <- subset(pheno, time != 'wk12')
+pheno <- pheno %>%
+  mutate(wk0.Delta_PASI = ifelse(time == 'wk0', Delta_PASI, 0),
+         wk1.Delta_PASI = ifelse(time == 'wk1', Delta_PASI, 0))
+t2g <- fread('Ensembl.Hs79.Tx.csv')
+e2g <- fread('Ensembl.Hs79.GeneSymbols.csv')
+
+# Pipeline
+for (tissue in c('Blood', 'LesionalSkin', 'NonlesionalSkin')) {
+
+  # TxImport
+  dir <- paste(getwd(), tissue, sep='/')
+  files <- file.path(dir, pheno$sample, 'MB.abundance.tsv')
+  txi <- tximport(files, type='kallisto', tx2gene=t2g, reader=fread)
+
+  # Normalise and filter data for SVA
+  dds <- DESeqDataSetFromTximport(txi, colData=pheno, design= ~ 1)
+  dds <- estimateSizeFactors(dds)
+  mat <- counts(dds, normalized=TRUE)
+  mat <- mat[rowMeans(mat) > 1, ] 
+
+  # Run SVA
+  mod <- model.matrix(~ 0 + subject + time + wk1.Delta_PASI, data=pheno)
+  mod0 <- model.matrix(~ 0 + subject + time, data=pheno)
+  svobj <- svaseq(mat, mod, mod0)
+  des <- cbind(mod, svobj$sv)
+
+  # Differential expression
+  dds <- estimateDispersions(dds, modelMatrix=des, maxit=1000)
+  dds <- nbinomWaldTest(dds, betaPrior=FALSE, modelMatrix=des, maxit=1000)
+  res <- data.frame(results(dds, name='wk1.Delta_PASI', filterfun=ihw))
+  eid <- rownames(res)
+  res <- res %>%
+    mutate(gene_id = eid) %>%
+    inner_join(e2g, by='gene_id') %>%
+    arrange(pvalue) %>%
+    select(gene_name, baseMean:padj)
+  write.csv(res, paste0('Baseline_', tissue, '.csv'))
+
+}
+
+
