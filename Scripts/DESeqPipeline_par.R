3f724087d9e18dfdeb0a37000f87b29174dbe93f
dswatson <david.s.watson11@gmail.com>
2016-10-02 10:44:52 +0100

    reformat, contrast matrices

--- Scripts/DESeqPipeline_par.R
+++ Scripts/DESeqPipeline_par.R
@@ -6,13 +6,7 @@ library(DESeq2)
 library(dplyr)
 
 # Prep data
-pheno <- fread(paste0(getwd(), '/Data/PrePSORT_Clinical.csv')) %>%
-  mutate(wk00 = ifelse(Time == 'wk00', 1, 0),
-         wk01 = ifelse(Time == 'wk01', 1, 0),
-         wk12 = ifelse(Time == 'wk12', 1, 0),
-         wk00.Response = ifelse(Time == 'wk00', DeltaPASI, 0),
-         wk01.Response = ifelse(Time == 'wk01', DeltaPASI, 0),
-         wk12.Response = ifelse(Time == 'wk12', DeltaPASI, 0))
+pheno <- fread(paste0(getwd(), '/Data/PrePSORT_Clinical.csv'))
 t2g <- fread(paste0(getwd(), '/Data/Ensembl.Hs79.Tx.csv'))
 e2g <- fread(paste0(getwd(), '/Data/Ensembl.Hs79.GeneSymbols.csv'))
 
@@ -28,31 +22,36 @@ loop <- function(tissue) {
   # TxImport
   dir <- paste0(getwd(), '/Data/', tissue)
   files <- file.path(dir, pheno$Sample, 'abundance.tsv')
-  txi <- tximport(files, type='kallisto', tx2gene=t2g, reader=fread)
-  dds <- DESeqDataSetFromTximport(txi, colData=pheno, design= ~ 1)
+  txi <- tximport(files, type = 'kallisto', tx2gene = t2g, reader = fread)
+  dds <- DESeqDataSetFromTximport(txi, colData = pheno, design = ~ 1)
   dds <- estimateSizeFactors(dds)
-  mat <- counts(dds, normalized=TRUE)
+  mat <- counts(dds, normalized = TRUE)
   keep <- rowMeans(mat) > 1
   mat <- mat[keep, ] 
   dds <- dds[keep, ]
 
   # SVA
-  mod <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A + 
-                      wk00.Response + wk01.Response + wk12.Response, data=pheno)
-  mod0 <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A, data=pheno)
+  mod <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A + Time:DeltaPASI,
+                      data = pheno)
+  mod0 <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A, 
+                       data = pheno)
   svobj <- svaseq(mat, mod, mod0)
   des <- cbind(mod, svobj$sv)
+  colnames(des)[8:ncol(des)] <- c('wk00.Response', 'wk01.Response', 'wk12.Response',
+                                  paste0('SV', 1:svobj$n.sv))
 
   # DEseq
-  dds1 <- estimateDispersions(dds, modelMatrix=des, maxit=10000)
-  dds1 <- nbinomWaldTest(dds1, betaPrior=FALSE, modelMatrix=des, maxit=10000)
+  dds <- estimateDispersions(dds, modelMatrix = des, maxit = 10000)
+  dds <- nbinomWaldTest(dds, modelMatrix = des, maxit = 10000, betaPrior = FALSE)
 
   # Extract
-  res <- data.frame(results(dds1, name='wk00.Response', filterfun=ihw))
+  res <- data.frame(results(dds, 
+                            name = 'wk00.Response', 
+                            filterfun = ihw))
   id <- rownames(res)
   res <- res %>%
     mutate(gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name, 
            AvgExpr    = baseMean,
@@ -61,16 +60,19 @@ loop <- function(tissue) {
            q.value    = padj) %>%
     arrange(p.value) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk00.txt'), sep='\t')
+  fwrite(res, paste0(getwd(), '/Results/DESeq/', 
+    tissue, ',wk00.txt'), sep = '\t')
 
   ### Week One ###
 
   # Extract
-  res <- data.frame(results(dds1, name='wk01.Response', filterfun=ihw))
+  res <- data.frame(results(dds, 
+                            name = 'wk01.Response', 
+                            filterfun = ihw))
   id <- rownames(res)
   res <- res %>%
     mutate(gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name, 
            AvgExpr    = baseMean,
@@ -79,16 +81,19 @@ loop <- function(tissue) {
            q.value    = padj) %>%
     arrange(p.value) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk01.txt'), sep='\t')
+  fwrite(res, paste0(getwd(), '/Results/DESeq/', 
+    tissue, ',wk01.txt'), sep = '\t')
 
   ### Week Twelve ###
 
   # Extract
-  res <- data.frame(results(dds1, name='wk12.Response', filterfun=ihw))
+  res <- data.frame(results(dds, 
+                            name = 'wk12.Response', 
+                            filterfun = ihw))
   id <- rownames(res)
   res <- res %>%
     mutate(gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name, 
            AvgExpr    = baseMean,
@@ -97,26 +102,19 @@ loop <- function(tissue) {
            q.value    = padj) %>%
     arrange(p.value) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk12.txt'), sep='\t')
+  fwrite(res, paste0(getwd(), '/Results/DESeq/', 
+    tissue, ',wk12.txt'), sep = '\t')
 
   ### One week change ###
 
-  # SVA
-  mod <- model.matrix(~ 0 + Subject + wk01 + wk12 + wk01.Response + wk12.Response, data=pheno)
-  mod0 <- model.matrix(~ 0 + Subject + wk01 + wk12, data=pheno)
-  svobj <- svaseq(mat, mod, mod0)
-  des <- cbind(mod, svobj$sv)
-
-  # DESeq
-  dds2 <- estimateDispersions(dds, modelMatrix=des, maxit=10000)
-  dds2 <- nbinomWaldTest(dds2, betaPrior=FALSE, modelMatrix=des, maxit=10000)
-
   # Extract
-  res <- data.frame(results(dds2, name='wk01.Response', filterfun=ihw))
+  res <- data.frame(results(dds, 
+                            contrast  = list('wk01.Response', 'wk00.Response'), 
+                            filterfun = ihw))
   id <- rownames(res)
   res <- res %>%
     mutate(gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name, 
            AvgExpr    = baseMean,
@@ -125,16 +123,39 @@ loop <- function(tissue) {
            q.value    = padj) %>%
     arrange(p.value) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk00-wk01.txt'), sep='\t')
+  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk00-wk01.txt'), sep = '\t')
+
+  ### Eleven Week Change ###
+
+  # Extract
+  res <- data.frame(results(dds, 
+                            contrast = list('wk12.Response', 'wk01.Response'), 
+                            filterfun = ihw))
+  id <- rownames(res)
+  res <- res %>%
+    mutate(gene_id = id) %>%
+    inner_join(e2g, by = 'gene_id') %>%
+    rename(EnsemblID  = gene_id,
+           GeneSymbol = gene_name, 
+           AvgExpr    = baseMean,
+           logFC      = log2FoldChange, 
+           p.value    = pvalue,
+           q.value    = padj) %>%
+    arrange(p.value) %>%
+    select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
+  fwrite(res, paste0(getwd(), '/Results/DESeq/', 
+    tissue, ',wk00-wk12.txt'), sep = '\t')
 
   ### Twelve Week Change ###
 
   # Extract
-  res <- data.frame(results(dds2, name='wk12.Response', filterfun=ihw))
+  res <- data.frame(results(dds, 
+                            contrast = list('wk12.Response', 'wk00.Response'), 
+                            filterfun = ihw))
   id <- rownames(res)
   res <- res %>%
     mutate(gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name, 
            AvgExpr    = baseMean,
@@ -143,41 +164,13 @@ loop <- function(tissue) {
            q.value    = padj) %>%
     arrange(p.value) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk00-wk12.txt'), sep='\t')
-
-  ### Eleven Week Change ###
-
-  # SVA
-  mod <- model.matrix(~ 0 + Subject + wk00 + wk12 + wk00.Response + wk12.Response, data=pheno)
-  mod0 <- model.matrix(~ 0 + Subject + wk00 + wk12, data=pheno)
-  svobj <- svaseq(mat, mod, mod0)
-  des <- cbind(mod, svobj$sv)
-
-  # DESeq
-  dds3 <- estimateDispersions(dds, modelMatrix=des, maxit=10000)
-  dds3 <- nbinomWaldTest(dds3, betaPrior=FALSE, modelMatrix=des, maxit=10000)
-
-  # Extract
-  res <- data.frame(results(dds3, name='wk12.Response', filterfun=ihw))
-  id <- rownames(res)
-  res <- res %>%
-    mutate(gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
-    rename(EnsemblID  = gene_id,
-           GeneSymbol = gene_name, 
-           AvgExpr    = baseMean,
-           logFC      = log2FoldChange, 
-           p.value    = pvalue,
-           q.value    = padj) %>%
-    arrange(p.value) %>%
-    select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk01-wk12.txt'), sep='\t')
+  fwrite(res, paste0(getwd(), '/Results/DESeq/', tissue, ',wk01-wk12.txt'), sep = '\t')
 
 }
 
 # Compute in parallel
 library(doParallel)
-registerDoParallel(cores=3)
-foreach(i=c('Blood', 'LesionalSkin', 'NonlesionalSkin')) %dopar% loop(i)
+registerDoParallel(cores = 3)
+foreach(i = c('Blood', 'LesionalSkin', 'NonlesionalSkin')) %dopar% loop(i)
 
 
--- Scripts/voomPipeline_par.R
+++ Scripts/voomPipeline_par.R
@@ -8,13 +8,7 @@ library(qvalue)
 library(dplyr)
 
 # Prep data
-pheno <- fread(paste0(getwd(), '/Data/PrePSORT_Clinical.csv')) %>%
-  mutate(wk00 = ifelse(Time == 'wk00', 1, 0),
-         wk01 = ifelse(Time == 'wk01', 1, 0),
-         wk12 = ifelse(Time == 'wk12', 1, 0),
-         wk00.Response = ifelse(Time == 'wk00', DeltaPASI, 0),
-         wk01.Response = ifelse(Time == 'wk01', DeltaPASI, 0),
-         wk12.Response = ifelse(Time == 'wk12', DeltaPASI, 0))
+pheno <- fread(paste0(getwd(), '/Data/PrePSORT_Clinical.csv'))
 t2g <- fread(paste0(getwd(), '/Data/Ensembl.Hs79.Tx.csv'))
 e2g <- fread(paste0(getwd(), '/Data/Ensembl.Hs79.GeneSymbols.csv'))
 
@@ -24,8 +18,8 @@ loop <- function(tissue) {
   # TxImport
   dir <- paste0(getwd(), '/Data/', tissue)
   files <- file.path(dir, pheno$Sample, 'abundance.tsv')
-  txi <- tximport(files, type='kallisto', tx2gene=t2g, reader=fread, 
-                  countsFromAbundance='lengthScaledTPM')
+  txi <- tximport(files, type = 'kallisto', tx2gene = t2g, reader = fread, 
+                  countsFromAbundance = 'lengthScaledTPM')
   keep <- rowSums(cpm(txi$counts) > 1) >= 3
   y <- DGEList(txi$counts[keep, ])
   y <- calcNormFactors(y)
@@ -33,134 +27,126 @@ loop <- function(tissue) {
   ### Week Zero ###
 
   # SVA
-  mod <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A + 
-                      wk00.Response + wk01.Response + wk12.Response, data=pheno)
-  mod0 <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A, data=pheno)
+  mod <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A + Time:DeltaPASI,
+                      data = pheno)
+  mod0 <- model.matrix(~ 0 + Time + Sex + Age + BMI + PASI_A, 
+                       data = pheno)
   svobj <- svaseq(cpm(y), mod, mod0)
   des <- cbind(mod, svobj$sv)
-  colnames(des)[11:ncol(des)] <- paste0('SV', 1:svobj$n.sv)
+  colnames(des)[8:ncol(des)] <- c('wk00.Response', 'wk01.Response', 'wk12.Response',
+                                  paste0('SV', 1:svobj$n.sv))
 
   # Voom
   v <- voom(y, des)
   fit <- lmFit(v, des)
-  fit <- eBayes(fit, robust=TRUE)
+  fit <- eBayes(fit, robust = TRUE)
 
   # Extract
-  top <- topTable(fit, coef='wk00.Response', number=Inf, sort.by='p')
+  top <- topTable(fit, coef = 'wk00.Response', number = Inf, sort.by = 'p')
   id <- rownames(top)
   top <- top %>%
     mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name,
            p.value    = P.Value, 
            AvgExpr    = AveExpr) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(top, paste0(getwd(), '/Results/voom/', tissue, ',wk00.txt'), sep='\t')
+  fwrite(top, paste0(getwd(), '/Results/voom/', 
+         tissue, ',wk00.txt'), sep = '\t')
 
   ### Week One ###
 
   # Extract
-  top <- topTable(fit, coef='wk01.Response', number=Inf, sort.by='p')
+  top <- topTable(fit, coef = 'wk01.Response', number = Inf, sort.by = 'p')
   id <- rownames(top)
   top <- top %>%
     mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name,
            p.value    = P.Value, 
            AvgExpr    = AveExpr) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(top, paste0(getwd(), '/Results/voom/', tissue, ',wk01.txt'), sep='\t')
+  fwrite(top, paste0(getwd(), '/Results/voom/', 
+         tissue, ',wk01.txt'), sep = '\t')
   
   ### Week Twelve ###
 
   # Extract
-  top <- topTable(fit, coef='wk12.Response', number=Inf, sort.by='p')
+  top <- topTable(fit, coef = 'wk12.Response', number = Inf, sort.by = 'p')
   id <- rownames(top)
   top <- top %>%
     mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name,
            p.value    = P.Value, 
            AvgExpr    = AveExpr) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(top, paste0(getwd(), '/Results/voom/', tissue, ',wk12.txt'), sep='\t')
+  fwrite(top, paste0(getwd(), '/Results/voom/', 
+         tissue, ',wk12.txt'), sep = '\t')
 
   ### One Week Change ###
 
-  # SVA
-  mod <- model.matrix(~ 0 + Subject + wk01 + wk12 + wk01.Response + wk12.Response, data=pheno)
-  mod0 <- model.matrix(~ 0 + Subject + wk01 + wk12, data=pheno)
-  svobj <- svaseq(cpm(y), mod, mod0)
-  des <- cbind(mod, svobj$sv)                                                           
-  colnames(des)[15:ncol(des)] <- paste0('SV', 1:svobj$n.sv) 
-
-  # Voom
-  v <- voom(y, des)
+  # Build contrasts
+  cm <- makeContrasts('Delta01' = wk01.Response - wk00.Response,
+                      'Delta11' = wk12.Response - wk01.Response,
+                      'Delta12' = wk12.Response - wk00.Response, levels=des)
   fit <- lmFit(v, des)
-  fit <- eBayes(fit, robust=TRUE)
+  fit <- contrasts.fit(fit, cm)
+  fit <- eBayes(fit, robust = TRUE)
 
   # Extract
-  top <- topTable(fit, coef='wk01.Response', number=Inf, sort.by='p')
+  top <- topTable(fit, coef = 'Delta01', number = Inf, sort.by = 'p')
   id <- rownames(top)
   top <- top %>%
     mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name,
            p.value    = P.Value, 
            AvgExpr    = AveExpr) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(top, paste0(getwd(), '/Results/voom/', tissue, ',wk00-wk01.txt'), sep='\t')
+  fwrite(top, paste0(getwd(), '/Results/voom/', 
+         tissue, ',wk00-wk01.txt'), sep = '\t')
+
+  ### Eleven Week Change ###
+
+  # Extract
+  top <- topTable(fit, coef = 'Delta11', number = Inf, sort.by = 'p')
+  id <- rownames(top)
+  top <- top %>%
+    mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
+    inner_join(e2g, by = 'gene_id') %>%
+    rename(EnsemblID  = gene_id,
+           GeneSymbol = gene_name,
+           p.value    = P.Value, 
+           AvgExpr    = AveExpr) %>%
+    select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
+  fwrite(top, paste0(getwd(), '/Results/voom/', 
+         tissue, ',wk00-wk12.txt'), sep = '\t')
 
   ### Twelve Week Change ###
 
   # Extract
-  top <- topTable(fit, coef='wk12.Response', number=Inf, sort.by='p')
+  top <- topTable(fit, coef = 'Delta12', number = Inf, sort.by = 'p')
   id <- rownames(top)
   top <- top %>%
     mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
+    inner_join(e2g, by = 'gene_id') %>%
     rename(EnsemblID  = gene_id,
            GeneSymbol = gene_name,
            p.value    = P.Value, 
            AvgExpr    = AveExpr) %>%
     select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(top, paste0(getwd(), '/Results/voom/', tissue, ',wk00-wk12.txt'), sep='\t')
-
-  ### Eleven Week Change ###
-
-  # SVA
-  mod <- model.matrix(~ 0 + Subject + wk00 + wk12 + wk00.Response + wk12.Response, data=pheno)
-  mod0 <- model.matrix(~ 0 + Subject + wk00 + wk12, data=pheno)
-  svobj <- svaseq(cpm(y), mod, mod0)
-  des <- cbind(mod, svobj$sv)                                                           
-  colnames(des)[15:ncol(des)] <- paste0('SV', 1:svobj$n.sv) 
-
-  # Voom
-  v <- voom(y, des)
-  fit <- lmFit(v, des)
-  fit <- eBayes(fit, robust=TRUE)
-
-  # Extract
-  top <- topTable(fit, coef='wk12.Response', number=Inf, sort.by='p')
-  id <- rownames(top)
-  top <- top %>%
-    mutate(q.value = qvalue(P.Value)$qvalues, gene_id = id) %>%
-    inner_join(e2g, by='gene_id') %>%
-    rename(EnsemblID  = gene_id,
-           GeneSymbol = gene_name,
-           p.value    = P.Value, 
-           AvgExpr    = AveExpr) %>%
-    select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value)
-  fwrite(top, paste0(getwd(), '/Results/voom/', tissue, ',wk01-wk12.txt'), sep='\t')
+  fwrite(top, paste0(getwd(), '/Results/voom/', 
+         tissue, ',wk01-wk12.txt'), sep = '\t')
 
 }
 
 # Compute in parallel
 library(doParallel)
-registerDoParallel(cores=3)
-foreach(i=c('Blood', 'LesionalSkin', 'NonlesionalSkin')) %dopar% loop(i)
+registerDoParallel(cores = 3)
+foreach(i = c('Blood', 'LesionalSkin', 'NonlesionalSkin')) %dopar% loop(i)
 
