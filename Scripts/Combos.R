cf1ea1d75bdf5e2f9c3a187a2a83d1e636cfae47
dswatson <david.s.watson11@gmail.com>
2016-11-15 12:06:31 +0000

    combos

---
+++ Scripts/Combos.R
@@ -0,0 +1,53 @@
+# Load libraries
+library(data.table)
+library(dplyr)
+
+# Combo function
+combo <- function(tissue) {
+  
+  # Load baseline
+  wk00 <- fread(paste0('./Results/Response/', tissue, '.wk00.Response.txt')) %>%
+            rename(p.value_wk00 = p.value,
+                   logFC_wk00   = logFC) %>%
+            select(EnsemblID, GeneSymbol, logFC_wk00, p.value_wk00)
+  
+  # Load deltas
+  delta01 <- fread(paste0('./Results/Time/', tissue, '.Delta01.txt')) %>%
+    rename(p.value_delta01 = p.value,
+           logFC_delta01   = logFC) %>%
+    select(EnsemblID, logFC_delta01, p.value_delta01)
+  delta12 <- fread(paste0('./Results/Time/', tissue, '.Delta12.txt')) %>%
+    rename(p.value_delta12 = p.value,
+           logFC_delta12   = logFC) %>%
+    select(EnsemblID, logFC_delta12, p.value_delta12)
+  
+  # Combos
+  mix01 <- inner_join(wk00, delta01, by = 'EnsemblID') %>%
+    rowwise() %>%
+    mutate(p.value_combo = pchisq(-2 * sum(log(c(p.value_delta01, 
+                                                 p.value_wk00))),
+                                  df = 4, lower = FALSE))
+  mix01$q.value <- p.adjust(mix01$p.value_combo, method = 'BH')
+  fwrite(mix01, paste0('./Results/Combos/',
+                       tissue, '.Combo_Baseline.Delta01.txt'), sep = '\t')
+  mix12 <- inner_join(wk00, delta12, by = 'EnsemblID') %>%
+    rowwise() %>%
+    mutate(p.value_combo = pchisq(-2 * sum(log(c(p.value_delta12, 
+                                                 p.value_wk00))),
+                                  df = 4, lower = FALSE))
+  mix12$q.value <- p.adjust(mix12$p.value_combo, method = 'BH')
+  fwrite(mix12, paste0('./Results/Combos/', 
+                       tissue, '.Combo_Baseline.Delta12.txt'), sep = '\t')
+  
+}
+
+# Run
+for (tissue in c('Blood', 'Lesional', 'Nonlesional')) combo(tissue)
+
+
+  
+  
+  
+  
+  
+  
