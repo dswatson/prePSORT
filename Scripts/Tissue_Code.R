682be178d0c96ad823a30ddfe866cf15cae3218e
dswatson <david.s.watson11@gmail.com>
2017-02-16 16:45:47 +0000

    tissue update

--- Scripts/Tissue_Code.R
+++ Scripts/Tissue_Code.R
@@ -1,41 +1,67 @@
-# Load libraries
+# Load libraries, set seed
 library(data.table)
 library(tximport)
 library(edgeR)
 library(limma)
+library(qvalue)
+library(qusage)
 library(dplyr)
+set.seed(123)
 
-# Prep data
-pheno <- fread('./Data/Clinical.csv') %>%
+# Import data
+clin <- fread('./Data/Clinical.csv') %>%
   mutate(Tissue = relevel(as.factor(Tissue), ref = 'Nonlesional'))
 t2g <- fread('./Data/Ensembl.Hs79.Tx.csv')
 e2g <- fread('./Data/Ensembl.Hs79.GeneSymbols.csv')
-
-# TxImport
-files <- file.path('./Data/RawCounts', pheno$Sample, 'abundance.tsv')
+mods <- fread('./Data/ChaussabelModules.csv')
+mod_list <- lapply(unique(mods$Module), function(m) mods[Module == m, gene_name])
+names(mod_list) <- unique(mods$Module)
+files <- file.path('./Data/RawCounts', clin$Sample, 'abundance.tsv')
 txi <- tximport(files, type = 'kallisto', tx2gene = t2g, reader = fread, 
                 countsFromAbundance = 'lengthScaledTPM')
-keep <- rowSums(cpm(txi$counts) > 1) >= 9
-y <- DGEList(txi$counts[keep, ])
+
+# Collapse, filter counts
+y <- avereps(txi$counts, ID = e2g[gene_id %in% rownames(txi$counts), gene_name])
+keep <- rowSums(cpm(y) > 1) >= 9
+y <- DGEList(y[keep, ])
 y <- calcNormFactors(y)
 
 # Fit model
-des <- model.matrix(~ 0 + Subject:Time + Tissue:Time, data = pheno)
+des <- model.matrix(~ 0 + Subject:Time + Tissue:Time, data = clin)
 v <- voomWithQualityWeights(y, des)
-idx <- rownames(v)
-fit <- eBayes(lmFit(v, des), robust = TRUE)
+fit <- eBayes(lmFit(v, des))
 
-# Export results
+# Genes
 topTable(fit, coef = 34, number = Inf, sort.by = 'none') %>%
-  mutate(gene_id = idx) %>%
-  inner_join(e2g, by = 'gene_id') %>%
-  rename(EnsemblID  = gene_id,
-         GeneSymbol = gene_name,
-         p.value    = P.Value,
-         q.value    = adj.P.Val,
-         AvgExpr    = AveExpr) %>%
+  rename(AvgExpr = AveExpr,
+         p.value = P.Value) %>%
+  mutate(q.value = qvalue(p.value)$qvalues,
+            Gene = rownames(v)) %>%
   arrange(p.value) %>%
-  select(EnsemblID, GeneSymbol, AvgExpr, logFC, p.value, q.value) %>%
-  fwrite('./Results/Tissue/LvsN_wk00.txt', sep = '\t')
+  mutate(Idx = row_number()) %>%
+  select(Idx, Gene, AvgExpr, logFC, p.value, q.value) %>%
+  fwrite('./Results/Tissue/LvsN_wk00.Genes.txt', sep = '\t')
 
+# Modules
+se <- sqrt(fit$s2.post) * fit$stdev.unscaled[, coef]
+sd.a <- se / (fit$sigma * fit$stdev.unscaled[, coef])
+sd.a[is.infinite(sd.a)] <- 1
+resid_mat <- residuals(fit, v)
+overlap <- sapply(mod_list, function(p) sum(p %in% rownames(v)))
+mod_list <- mod_list[overlap > 0]
+res <- newQSarray(mean = fit$coefficients[, coef],       # Create QSarray obj
+                    SD = se,
+              sd.alpha = sd.a,
+                   dof = fit$df.total,
+                labels = rep('resid', ncol(v)))
+res <- aggregateGeneSet(res, mod_list, n.points = 2^14)  # PDF per gene set
+res <- calcVIF(resid_mat, res, useCAMERA = FALSE)        # VIF on resid_mat
+qsTable(res, number = Inf, sort.by = 'p') %>%
+  rename(p.value = p.Value,
+         Module = pathway.name,
+         logFC = log.fold.change) %>%
+  mutate(q.value = qvalue(p.value)$qvalues,
+         Idx = row_number()) %>%
+  select(Idx, Module:p.value, q.value) %>%
+  fwrite('./Results/Tissue/LvsN_wk00.Modules.txt', sep = '/t')
 
