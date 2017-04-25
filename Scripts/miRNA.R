e9c530bac00ac952aa85fb63d580c81498313b37
dswatson <david.s.watson11@gmail.com>
2017-04-25 19:54:54 +0100

    multiplat

--- Scripts/miRNA.R
+++ Scripts/miRNA.R
@@ -1,5 +1,6 @@
 # Load libraries
 library(data.table)
+library(edgeR)
 library(limma)
 library(qvalue)
 library(dplyr)
@@ -8,10 +9,13 @@ library(dplyr)
 clin <- fread('./Data/Clinical.csv') %>%
   filter(Tissue == 'Blood') %>%
   mutate(Sample = gsub('.Blood.', '_', Sample))
-mat <- read.csv('./Data/SomaScan.csv', row.names = 1) %>% 
+mat <- read.csv('./Data/miRNA.csv', row.names = 1) %>% 
   as.matrix()
-mat <- log2(mat)
-mat <- mat[, match(clin$Sample, colnames(mat))]
+
+# Collapse, filter counts
+keep <- rowSums(cpm(mat) > 1) >= 3
+y <- DGEList(mat[keep, ])
+y <- calcNormFactors(y)
 
 # Winsorise delta PASI distribution
 winsorise <- function(x, multiple = 2) {
@@ -27,23 +31,26 @@ clin <- clin %>% mutate(DeltaPASI = winsorise(DeltaPASI))
 # Define results function
 res <- function(contrast) {
   topTable(fit, number = Inf, sort.by = 'none', coef = contrast) %>%
-    mutate(Analyte = rownames(mat),
-           q.value = qvalue(P.Value)$qvalues) %>%
+    mutate(Gene = rownames(y),
+        q.value = qvalue(P.Value)$qvalues) %>%
     rename(p.value = P.Value,
            AvgExpr = AveExpr) %>%
     arrange(p.value) %>%
-    select(Analyte, AvgExpr, logFC, p.value, q.value) %>%
-    fwrite(paste0('./Results/Response/Proteomics/', 
+    select(Gene, AvgExpr, logFC, p.value, q.value) %>%
+    fwrite(paste0('./Results/Response/miRNA/', 
                   paste0(contrast, '.txt')), sep = '\t')
 }
 
 # Fit model
 des <- model.matrix(~ 0 + Time + Time:DeltaPASI, data = clin)
 colnames(des)[4:6] <- c(paste(unique(clin$Time), 'Response', sep = '.'))
-icc <- duplicateCorrelation(mat, des, block = clin$Subject)
-fit <- lmFit(mat, des, correlation = icc$cor, block = clin$Subject)
+v <- voomWithQualityWeights(y, des)
+icc <- duplicateCorrelation(v, des, block = clin$Subject)
+v <- voomWithQualityWeights(y, des, correlation = icc$cor,
+                            block = clin$Subject)
+icc <- duplicateCorrelation(v, des, block = clin$Subject)
+fit <- lmFit(v, des, correlation = icc$cor, block = clin$Subject)
 fit <- eBayes(fit)
 for (j in colnames(des)[4:6]) res(j)
-saveRDS(mat, './Data/mat_prot.rds')
-saveRDS(fit, './Data/fit_prot.rds')
-
+saveRDS(v$E, './Data/mat_miRNA.rds')
+saveRDS(fit, './Data/fit_miRNA.rds')
