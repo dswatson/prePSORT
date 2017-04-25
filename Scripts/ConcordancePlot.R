415c237355fc7a68bb3c46688aa39414b7cf09a5
dswatson <david.s.watson11@gmail.com>
2017-04-26 00:47:06 +0100

    sup, unsup

--- Scripts/ConcordancePlot.R
+++ Scripts/ConcordancePlot.R
@@ -1,71 +1,59 @@
 # Load libraries
-library(corrplot)
-
-# Import cluster assignments
-df <- read.csv('./Results/ConsensusClusters/clusters.csv', row.names = 1)
-
-# Create p-value matrix
-mat <- matrix(nrow = ncol(df), ncol = ncol(df), 
-              dimnames = list(colnames(df), colnames(df)))
-dimnames(mat) <- lapply(dimnames(mat), function(i) gsub('.Response', '', i))
-for (i in 2:nrow(mat)) {
-  for (j in 1:(i - 1)) {
-    mat[i, j] <- fisher.test(df[[i]], df[[j]])$p.value
-  }
-}
-
-# Transform, fill NAs
-mat <- -log10(mat)
-mat[is.na(mat)] <- ceiling(max(mat, na.rm = TRUE))
-
-# Plot
-corrplot(mat, is.corr = FALSE, type = 'lower', diag = FALSE, method = 'shade', 
-         title = 'Cluster Concordance', addgrid.col = 'black', mar = c(0, 0, 1, 0), 
-         tl.cex = 0.75, tl.col = 'black', tl.srt = 45, cl.lim = c(0, 3))
-
-
-
-
-
-### Using mutual information ###
-
-# Load libraries
+library(data.table)
 library(infotheo)
 library(corrplot)
+library(dplyr)
 
-# Import cluster assignments
+# Import clinical stratifier
 clin <- fread('./Data/Clinical.csv') %>%
-  select(Subject, PASI_75) %>%
-  unique()
-df <- read.csv('./Results/ConsensusClusters/clusters_km.csv', row.names = 1) %>%
+  distinct(Subject, PASI_75) 
+
+
+### SUPERVISED ###
+
+# Load cluster assignments
+df <- read.csv('./Results/Clusters/Supervised/PAM.csv', row.names = 1) %>%
   mutate(PASI_75 = clin$PASI_75)
 
-# Create p-value matrix
+# Create mutual information matrix
 mat <- matrix(nrow = ncol(df), ncol = ncol(df), 
               dimnames = list(colnames(df), colnames(df)))
-dimnames(mat) <- lapply(dimnames(mat), function(i) gsub('.Response', '', i))
 for (i in 2:nrow(mat)) {
   for (j in 1:(i - 1)) {
     mat[i, j] <- mutinformation(df[[i]], df[[j]])
   }
 }
 mat <- natstobits(mat)
-
-# Fill NAs
-#mat[is.na(mat)] <- ceiling(max(mat, na.rm = TRUE))
 mat[is.na(mat)] <- max(mat, na.rm = TRUE)
 
 # Plot
-corrplot(mat, is.corr = FALSE, type = 'lower', diag = FALSE, method = 'shade', 
-         title = 'Cluster Concordance', addgrid.col = 'black', mar = c(0, 0, 1, 0), 
-         tl.cex = 0.75, tl.col = 'black', tl.srt = 45, cl.lim = c(0, 1))
+corrplot(mat, method = 'shade', is.corr = FALSE, type = 'lower', diag = FALSE,  
+         title = 'Cluster Concordance: Supervised', mar = c(0, 1, 1, 0),
+         addgrid.col = 'black', addCoef.col = 'black',
+         tl.cex = 0.65, tl.col = 'black', tl.srt = 45, cl.lim = c(0, 1))
 
 
+### UNSUPERVISED ###
 
+# Load cluster assignments
+df <- read.csv('./Results/Clusters/Unsupervised/PAM.csv', row.names = 1) %>%
+  mutate(PASI_75 = clin$PASI_75)
 
-### TO DO:
-# Rerun ConsensusClustering with maxK = 4
-# Baseline numbers for all platforms and PASI75
-# Mutual info with scale up to max possible MI given n and k
+# Create mutual information matrix
+mat <- matrix(nrow = ncol(df), ncol = ncol(df), 
+              dimnames = list(colnames(df), colnames(df)))
+for (i in 2:nrow(mat)) {
+  for (j in 1:(i - 1)) {
+    mat[i, j] <- mutinformation(df[[i]], df[[j]])
+  }
+}
+mat <- natstobits(mat)
+mat[is.na(mat)] <- max(mat, na.rm = TRUE)
+
+# Plot
+corrplot(mat, method = 'shade', is.corr = FALSE, type = 'lower', diag = FALSE,  
+         title = 'Cluster Concordance: Unsupervised', mar = c(0, 1, 1, 0),
+         addgrid.col = 'black', addCoef.col = 'black',
+         tl.cex = 0.65, tl.col = 'black', tl.srt = 45, cl.lim = c(0, 1))
 
 
