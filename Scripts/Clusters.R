ed3fff30783b0ada4d5d1bebac0fed5e2ce13d96
dswatson <david.s.watson11@gmail.com>
2017-05-19 16:07:59 +0100

    unbiased estimates

--- Scripts/Clusters.R
+++ Scripts/Clusters.R
@@ -3,62 +3,94 @@
 # Load libraries, register cores, set seed
 library(data.table)
 library(limma)
+library(edgeR)
 library(wordspace)
 library(Rtsne)
 library(cluster)
 library(dplyr)
 library(doMC)
-registerDoMC(5)
+registerDoMC(4)
 set.seed(123)
 
-# Create summary data frame
-out <- fread('./Data/Clinical.csv') %>%
-  distinct(Subject) 
+# Define continuous response
+clin <- fread('./Data/Clin_Baseline.csv')
+winsorise <- function(x, multiple = 2) {
+  y <- x - median(x)
+  lim <- mad(y, center = 0) * multiple
+  y[y > lim] <- lim
+  y[y < -lim] <- -lim
+  y <- y + median(x)
+  return(y)
+}
+clin <- clin %>% mutate(DeltaPASI = winsorise(DeltaPASI))
+y <- clin$DeltaPASI
+
+# Create output data frame
+out <- clin %>% select(Subject)
+
+# Import data
+mRNA <- readRDS('./Data/mRNA_RawCounts.rds')
+mRNA <- mRNA[, grepl('wk00', colnames(mRNA))]
+miRNA <- readRDS('./Data/miRNA_RawCounts.rds')
+miRNA <- miRNA[, grepl('wk00', colnames(miRNA))]
+prot <- readRDS('./Data/Prot_RawDat.rds')
+prot <- prot[, grepl('wk00', colnames(prot))]
 
 # Define clusters function
-clusters <- function(plat, supervised = TRUE) {
+clusters <- function(data_type, supervised = TRUE) {
   
-  # Load data
-  if (grepl('mRNA', plat)) {
-    mat <- readRDS('./Data/mat_mRNA.rds')
-    fit <- readRDS('./Data/fit_mRNA.rds')
-    clin <- fread('./Data/Clinical.csv')
-    if (plat == 'mRNA_Blood') {
-      coef <- colnames(fit)[grepl('Blood.wk00', colnames(fit))]
-      samples <- grep('Blood.wk00', colnames(mat))
-    } else if (plat == 'mRNA_Lesional') {
-      coef <- colnames(fit)[grepl('Lesional.wk00', colnames(fit))]
-      samples <- grep('Lesional.wk00', colnames(mat))
-    } else if (plat == 'mRNA_Nonlesional') {
-      coef <- colnames(fit)[grepl('Nonlesional.wk00', colnames(fit))]
-      samples <- grep('Nonlesional.wk00', colnames(mat))
-    }
-  } else if (plat == 'miRNA_Blood') {
-    mat <- readRDS('./Data/mat_miRNA.rds')
-    fit <- readRDS('./Data/fit_miRNA.rds')
-    coef <- colnames(fit)[grepl('wk00.Response', colnames(fit))]
-    samples <- grep('wk00', colnames(mat))
-  } else if (plat == 'Prot_Blood') {
-    mat <- readRDS('./Data/mat_prot.rds')
-    fit <- readRDS('./Data/fit_prot.rds')
-    coef <- colnames(fit)[grepl('wk00.Response', colnames(fit))]
-    samples <- grep('wk00', colnames(mat))
+  # Normalize, transform data
+  if (data_type == 'Blood_mRNA') {
+    mat <- mRNA[, grepl('Blood', colnames(mRNA))]
+    keep <- rowSums(cpm(mat) > 1) >= 3
+    mat <- DGEList(mat[keep, ])
+    mat <- calcNormFactors(mat)
+  } else if (data_type == 'Lesional_mRNA') {
+    mat <- mRNA[, grepl('Lesional', colnames(mRNA))]
+    keep <- rowSums(cpm(mat) > 1) >= 3
+    mat <- DGEList(mat[keep, ])
+    mat <- calcNormFactors(mat)
+  } else if (data_type == 'Nonlesional_mRNA') {
+    mat <- mRNA[, grepl('Nonlesional', colnames(mRNA))]
+    keep <- rowSums(cpm(mat) > 1) >= 3
+    mat <- DGEList(mat[keep, ])
+    mat <- calcNormFactors(mat)
+  } else if (data_type == 'Blood_miRNA') {
+    mat <- miRNA
+    keep <- rowSums(cpm(mat) > 1) >= 3
+    mat <- DGEList(mat[keep, ])
+    mat <- calcNormFactors(mat)
+  } else if (data_type == 'Blood_Proteomics') {
+    mat <- log2(prot)
   }
   
-  # Median centre data, filter 
-  mat <- mat[, samples]
-  mat <- sweep(mat, 1, apply(mat, 1, median))
+  # Filter, median centre data
   top <- round(0.5 * nrow(mat))
-  if (supervised) {
-    hits <- topTable(fit, coef = coef, number = top, sort.by = 'p')
+  if (supervised) {                              # By limma p-value
+    des <- model.matrix(~ y)
+    if (grepl('RNA', data_type)) {
+      mat <- voom(mat, des)
+    } 
+    fit <- eBayes(lmFit(mat, des))
+    hits <- topTable(fit, coef = 'y', number = top, sort.by = 'p')
     hits <- rownames(hits)
-    dm <- dist.matrix(t(mat[hits, ]), method = 'euclidean')
-  } else {
+    mat <- mat[hits, ]
+    if (grepl('RNA', data_type)) {
+      mat <- mat$E
+    }
+    mat <- sweep(mat, 1, apply(mat, 1, median))
+    dm <- dist.matrix(t(mat), method = 'euclidean')
+  } else {                                       # By leading fold change
+    if (grepl('RNA', data_type)) {
+      mat <- cpm(mat, log = TRUE, prior.count = 1)
+    }
+    mat <- sweep(mat, 1, apply(mat, 1, median))
     dm <- matrix(nrow = ncol(mat), ncol = ncol(mat))
     for (i in 2:ncol(mat)) {
       for (j in 1:(i - 1)) {
-        hits <- order((dat[, i] - dat[, j])^2, decreasing = TRUE)[1:top]
-        dm[i, j] <- sqrt(sum((dat[hits, i] - dat[hits, j])^2))
+        top_idx <- nrow(mat) - top + 1
+        dm[i, j] <- sqrt(sum(sort.int((mat[, i] - mat[, j])^2,
+                                      partial = top_idx)[top_idx:nrow(mat)]))
       }
     }
   }
@@ -70,20 +102,20 @@ clusters <- function(plat, supervised = TRUE) {
   # Run PAM
   out$new <- pam(tsne$Y, k = 2, cluster.only = TRUE)
   out$new <- paste0('C', out$new)
-  colnames(out)[colnames(out) == 'new'] <- plat
+  colnames(out)[colnames(out) == 'new'] <- data_type
   return(out)
   
 }
 
 # Execute in parallel
 combo <- function(x, y) inner_join(x, y, by = 'Subject')
-plats <- c('mRNA_Blood', 'mRNA_Lesional', 'mRNA_Nonlesional',
-           'miRNA_Blood', 'Prot_Blood')
-foreach(plat = plats, .combine = combo) %dopar% 
-  clusters(plat, supervised = TRUE) %>%
-  fwrite('./Results/Clusters/Supervised.csv')
-foreach(plat = plats, .combine = combo) %dopar% 
-  clusters(plat, supervised = FALSE) %>%
-  fwrite('./Results/Clusters/Unsupervised.csv')
+data_types <- c('Blood_Proteomics', 'Blood_miRNA',
+                'Blood_mRNA', 'Lesional_mRNA', 'Nonlesional_mRNA')
+foreach(d = data_types, .combine = combo) %dopar% 
+  clusters(d, supervised = TRUE) %>%
+  fwrite('./Results/SupervisedClusters.csv')
+foreach(d = data_types, .combine = combo) %dopar% 
+  clusters(d, supervised = FALSE) %>%
+  fwrite('./Results/UnsupervisedClusters.csv')
 
 
